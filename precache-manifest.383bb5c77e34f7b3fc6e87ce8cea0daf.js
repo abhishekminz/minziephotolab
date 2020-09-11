a036160cc28ca80584fcb356ce3645e4cd4f7abe
Abhishek Minz <51510507+abhishekminz@users.noreply.github.com>
2020-09-11 18:53:39 +0530

    Updates

---
+++ precache-manifest.383bb5c77e34f7b3fc6e87ce8cea0daf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3132dd0946d1ed52e4fd4d31649c190",
+    "url": "/minziephotolab/index.html"
+  },
+  {
+    "revision": "0353facb61615f793608",
+    "url": "/minziephotolab/static/css/main.01d31d61.chunk.css"
+  },
+  {
+    "revision": "2d8b12ecb67330759f6d",
+    "url": "/minziephotolab/static/js/2.5e0bed9e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/minziephotolab/static/js/2.5e0bed9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0353facb61615f793608",
+    "url": "/minziephotolab/static/js/main.06f3f250.chunk.js"
+  },
+  {
+    "revision": "63e58a76c184d20a8145",
+    "url": "/minziephotolab/static/js/runtime-main.29dbd814.js"
+  },
+  {
+    "revision": "309dafc9fbd0a8ba11a9bcbf3e29409f",
+    "url": "/minziephotolab/static/media/slide_one.309dafc9.jpg"
+  },
+  {
+    "revision": "ddd7f5b09494d58e9e6d09e3ac8a651b",
+    "url": "/minziephotolab/static/media/slide_three.ddd7f5b0.jpg"
+  },
+  {
+    "revision": "c3781acedbe6b68a25cecc94ed053cb3",
+    "url": "/minziephotolab/static/media/slide_two.c3781ace.jpg"
+  }
+]);
